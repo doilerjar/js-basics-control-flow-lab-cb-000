20798cbe5249d90b471e04667d7b7dd12da8a6e6
Kevin K <doilerjar@outlook.com>
2019-03-10 02:10:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-  if someValue > 0 && someValue <= 400 {
+  if (someValue > 0 && someValue <= 400) {
     return "This one is on me!"
   }
 }
