4bcf5e9fd07be200d0b0c6100a4de287ed6c02b8
Kevin K <doilerjar@outlook.com>
2019-03-10 02:10:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function scuberGreetingForFeet(someValue) {
   if (someValue > 0 && someValue <= 400) {
     return "This one is on me!"
   }
+  else if (someValue > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
 }
